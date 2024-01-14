634ed045b4af32e9e019645ad96435555b31c63e
Chris O'Brian <chris.obrian@nisiqtu.com>
2024-01-13 21:12:02 -0800

    set the default colorscheme in the init file

--- init.lua
+++ init.lua
@@ -317,6 +317,7 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- set the default colorscheme here
 vim.cmd.colorscheme 'night-owl'
 
 -- [[ Highlight on yank ]]
